63ca3392b6a06ce13a2554ae49431d7e19d7b908
Kjusok <103447181+Kjusok@users.noreply.github.com>
2023-06-29 18:12:14 +0300

    Fixed bug + added soundtrack
    Fixed bug + added soundtrack

---
+++ Assets/Scripts/Audio/StartMenuSoundTrack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class StartMenuSoundTrack : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _soundtrack;
+
+        private void Start()
+        {
+            _soundtrack.Play();
+        }
+    }
+}
